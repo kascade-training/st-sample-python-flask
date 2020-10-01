65f0c2136e6537fde6a8e5737fb065f6a60684c8
Ahmed HOSNI <hosniah@users.noreply.github.com>
2020-10-01 22:31:32 +0100

    Update test.py

--- test.py
+++ test.py
@@ -25,4 +25,5 @@ class TestHello(unittest.TestCase):
         self.assertIn("{name}", rv.data)
 
 if __name__ == '__main__':
-    unittest.main()
+    import xmlrunner
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
